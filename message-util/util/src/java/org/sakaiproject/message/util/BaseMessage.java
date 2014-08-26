f4c74f2eddf64bacbcefd54aa25eaf70f307132e
Nick Wilson <nick.wilson@it.ox.ac.uk>
2014-08-26 17:36:09 +0100

    WL-1026 : A msg is public if it is public or its site is
    Announcements made on a .anon site should be public by default.
    
    Actually the code uses reverse logic : a msg is not public if it is not in a public site and it is not public
    
    WL-1026 : doing a security check on channel
    
    WL-1026 : Remove unused import
    
    WL-1026 : Do security check in BaseMessage [message-util].
    
    WL-1026 : Use reference for channel rather than depending on Site API.
    
    WL-1026 : use m_userDirectoryService instance instead of static class

--- message-util/util/src/java/org/sakaiproject/message/util/BaseMessage.java
+++ message-util/util/src/java/org/sakaiproject/message/util/BaseMessage.java
@@ -2284,7 +2284,10 @@ public abstract class BaseMessage implements MessageService, DoubleStorageUser
 		 */
 		public List getMessagesPublic(Filter filter, boolean ascending)
 		{
-			return findFilterMessages(new PublicFilter(filter), ascending);
+			String anncRead = eventId(MessageService.SECURE_READ);
+			boolean isChannelPublic = m_securityService.unlock(m_userDirectoryService.getAnonymousUser(), anncRead, this.getReference());
+
+			return findFilterMessages(new PublicFilter(filter, isChannelPublic), ascending);
 
 		} // getMessagesPublic
 
@@ -4499,15 +4502,20 @@ public abstract class BaseMessage implements MessageService, DoubleStorageUser
 		/** The other filter to check with. May be null. */
 		protected Filter m_filter = null;
 
+		/** The site. May be public. */
+		protected boolean m_isChannelPublic;
+
 		/**
 		 * Constructor
-		 * 
-		 * @param filter
+		 *  @param filter
 		 *        The other filter we check with.
+		 * @param isChannelPublic
+		 *        Whether the channel is public or not
 		 */
-		public PublicFilter(Filter filter)
+		public PublicFilter(Filter filter, boolean isChannelPublic)
 		{
 			m_filter = filter;
+			m_isChannelPublic = isChannelPublic;
 
 		} // PublicFilter
 
@@ -4522,8 +4530,9 @@ public abstract class BaseMessage implements MessageService, DoubleStorageUser
 			if (o instanceof Message)
 			{
 				Message msg = (Message) o;
-				if (msg.getProperties().getProperty(ResourceProperties.PROP_PUBVIEW) == null ||
-					 !msg.getProperties().getProperty(ResourceProperties.PROP_PUBVIEW).equals(Boolean.TRUE.toString()))
+				if ((msg.getProperties().getProperty(ResourceProperties.PROP_PUBVIEW) == null ||
+						!msg.getProperties().getProperty(ResourceProperties.PROP_PUBVIEW).equals(Boolean.TRUE.toString()))
+						&& !m_isChannelPublic)
 					return false;
 				else if (msg.getHeader().getDraft())
 					return false;
